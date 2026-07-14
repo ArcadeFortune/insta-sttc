e46cb0a62048032a09e357a36c184822fca3f446
Alessio Lama <alessionights@gmail.com>
2026-07-14 10:50:40 +0200

    can show live base64 encoding of microphone input

--- src/main.c
+++ src/main.c
@@ -2,43 +2,31 @@
 
 int main()
 {
-  // float *buf = malloc(MIC_DATA_BUFFER_SIZE * sizeof(float));
-  // int16_t *pcmBuf = malloc(MIC_DATA_BUFFER_SIZE * sizeof(int16_t) * 10);
-  // char *base64Buf = malloc(MIC_DATA_BUFFER_SIZE * sizeof(char) * 10);
-  float *buf = malloc(6);
-  int16_t *pcmBuf = malloc(6);
-  char *base64Buf = malloc(6 * 4 / 3);
+  float *buf = malloc(MIC_DATA_BUFFER_SIZE * sizeof(float));
+  int16_t *pcmBuf = malloc(MIC_DATA_BUFFER_SIZE * sizeof(int16_t));
+  char *base64Buf = malloc(MIC_DATA_BUFFER_SIZE * sizeof(char) * 2 * 4 / 3);
 
-  buf[0] = 1.0f;
-  buf[1] = -1.0f;
-  buf[2] = .5f;
-  buf[3] = .15f;
-  buf[4] = 0;
-  buf[5] = 1.5f;
-
+  PaStream *stream = NULL;
   MicData micData = {
       .buf = buf,
       .pcmBuf = pcmBuf,
       .base64Buf = base64Buf,
-      .writeIndex = 6,
+      .writeIndex = 0,
       .readIndex = 0,
   };
-  processSample(&micData);
-  return 0;
-  // PaStream *stream = NULL;
 
-  // startListen(&micData, &stream);
+  startListen(&micData, &stream);
 
-  // for (int i = 0; i < 100; i++)
-  // {
-  //   processSample(&micData);
-  //   Pa_Sleep(10);
-  // }
-  // Pa_Sleep(20000);
+  while (1)
+  {
+    processSample(&micData);
+    Pa_Sleep(20);
+  }
 
-  // free(buf);
-  // free(base64Buf);
-  // stopListen(stream);
-  // printf("bye.\n");
+  free(buf);
+  free(pcmBuf);
+  free(base64Buf);
+  stopListen(stream);
+  printf("bye.\n");
   return 0;
 }
